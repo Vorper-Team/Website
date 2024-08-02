5be58f1b41b5dfd25797238cdaa8a351972fc261
PH7N <120841550+PH7N@users.noreply.github.com>
2024-08-02 16:58:33 +0500

    feat `docusaurus.config.ts`: Add 2 items to footer & prepare deployment

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -3,29 +3,22 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
+  // Root Info
   title: "Vorper",
   tagline:
     "A team of Developers from all around the world with a single goal: to make free and open source software.",
   favicon: "img/favicon.ico",
-
-  // Set the production url of your site here
   url: "https://www.vorper.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Vorper-Team", // Usually your GitHub org/user name.
-  projectName: "Vorper", // Usually your repo name.
-
+  // Website Deployment
+  organizationName: "Vorper-Team",
+  projectName: "Vorper",
+  deploymentBranch: "deployment",
   trailingSlash: false,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -37,17 +30,11 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/Vorper-Team/Vorper/tree/main/website/",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/Vorper-Team/Vorper/tree/main/website/",
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -57,7 +44,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: "img/vorper-card.png",
     navbar: {
       logo: {
@@ -94,6 +80,10 @@ const config: Config = {
               label: "Discord",
               href: "https://discord.gg/XeRaBUvRfe",
             },
+            {
+              label: "Email",
+              href: "mailto:contact@vorper.com",
+            },
           ],
         },
         {
@@ -106,6 +96,10 @@ const config: Config = {
             {
               label: "Terms of Service",
               href: "/terms",
+            },
+            {
+              label: "Branding",
+              href: "/branding",
             },
           ],
         },
