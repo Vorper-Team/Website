2a0f67a0c78fc87adca25abf6502f291e767aca7
PH7N <120841550+PH7N@users.noreply.github.com>
2024-08-04 10:55:46 +0500

    🐞 fix(teampage.css, team.tsx): Fix typos and format
    Fix a typo in the team page's head description and format everything with prettier

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -5,7 +5,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title="Team"
-      description="We are a team of developers from all around the world and this page will show you who our developers are.."
+      description="We are a team of developers from all around the world and this page will show you who our developers are."
     >
       <Team />
     </Layout>
